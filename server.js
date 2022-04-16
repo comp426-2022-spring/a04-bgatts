5fcf5266e79266541b308ff801c0b47fb500a75b
bgatts <59676688+bgatts@users.noreply.github.com>
2022-04-16 15:37:02 -0400

    help functionality enabled

--- server.js
+++ server.js
@@ -11,7 +11,29 @@ import Database from "better-sqlite3"
 
 const app = express()
 const args = minimist(process.argv.slice(2))
+
+
 args["port"]
+args["help"]
+
+if (args["help"] || args['h']){
+    let helpMessage = `--port	Set the port number for the server to listen on. Must be an integer
+    between 1 and 65535.
+
+    --debug	If set to true, creates endlpoints /app/log/access/ which returns
+    a JSON access log from the database and /app/error which throws 
+    an error with the message "Error test successful." Defaults to 
+    false.
+
+    --log		If set to false, no log files are written. Defaults to true.
+    Logs are always written to database.`
+
+    console.log(helpMessage)
+    process.exit(0)
+}
+
+
+
 
 
 const db = new Database('log.db')                 //set up database
